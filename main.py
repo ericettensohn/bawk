93336479a1056fea084f49aa41eda10f92bd503a
ericettensohn <ericettensohn@gmail.com>
2016-10-08 15:23:18 -0400

    Trending users

--- main.py
+++ main.py
@@ -33,14 +33,14 @@ def register_submit():
 
 
 	if check_username_result is None:
-		email = data['email']
+		avatar = data['avatar']
 		password = data['password'].encode('utf-8')
 		hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
-		username_insert_query = "INSERT INTO user (username, password, email) VALUES ('%s', '%s', '%s')" % (username, hashed_password, email)
+		username_insert_query = "INSERT INTO user (username, password, avatar) VALUES ('%s', '%s', '%s')" % (username, hashed_password, avatar)
 
 		cursor.execute(username_insert_query)
 		conn.commit()
-		return "you are logged in"
+		return "reg successful"
 	else:
 		print "username taken"
 		return redirect('/register?username=taken')
@@ -66,21 +66,78 @@ def login_submit():
 
 	check_password_result = cursor.fetchone()
 
-	# to check a hash against english
-	# if bcrypt.hashpw(password, check_password_result[0]) == check_password_result:
-	# 	print "match!"
+	get_avatar_query = "SELECT avatar FROM user WHERE username = '%s'" % username
+	cursor.execute(get_avatar_query)
+	get_avatar_result = cursor.fetchone()
 
 	if bcrypt.hashpw(password.encode('utf-8'), check_password_result[0].encode('utf-8')) == check_password_result[0].encode('utf-8'):
-		return "true"
+		return jsonify(get_avatar_result[0])
 
-@app.route('/get_posts')
+@app.route('/get_posts', methods=['POST'])
 def get_posts():
-	get_posts_query = "SELECT posts.avatar, username, content, votes, time, location FROM posts JOIN user ON uid = user.id"
+	data = request.get_json()
+	username = data['username']
+
+	get_user_id_query = "SELECT id FROM user WHERE username = '%s'" % username
+	cursor.execute(get_user_id_query)
+	user_id = cursor.fetchone()
+	# get_posts_query = "SELECT user.avatar, username, content, votes, time, location FROM posts JOIN user ON uid = user.id"
+	get_posts_query = "SELECT user.avatar, username, content, votes, time, location, uid FROM posts LEFT JOIN user ON uid = user.id LEFT JOIN follow on following_id = posts.uid WHERE follow.following_id IN (SELECT following_id from follow where follower_id = %s) GROUP BY user.avatar, username, content, votes, time, location, uid" % user_id
+
 	cursor.execute(get_posts_query)
 	get_posts_result = cursor.fetchall()
+	conn.commit()
 	return jsonify(get_posts_result)
 
+@app.route('/new_post', methods=['POST'])
+def new_post():
+	data = request.get_json()
+	new_post_content = data['newPostContent']
+	username = data['username']
 
+	get_user_id_query = "SELECT id FROM user WHERE username = '%s'" % username
+	cursor.execute(get_user_id_query)
+	get_user_id_result = cursor.fetchone()
+
+	insert_post_query = "INSERT INTO posts (content, uid) VALUES ('%s', '%s')" % (new_post_content, get_user_id_result[0])
+	print insert_post_query
+	cursor.execute(insert_post_query)
+	conn.commit()
+	return "success"
+
+@app.route('/get_trending_users', methods=['POST'])
+def get_trending_users():
+	data = request.get_json()
+	username = data['username']
+
+	get_user_id_query = "SELECT id FROM user WHERE username = '%s'" % username
+	cursor.execute(get_user_id_query)
+	user_id = cursor.fetchone()
+
+	# get_trending_users_query = "SELECT id, avatar, username FROM user WHERE username != '%s'" % username
+
+	get_trending_users_query = "SELECT user.id, avatar, username FROM user LEFT JOIN follow on following_id = user.id WHERE follow.following_id NOT IN (SELECT following_id from follow where follower_id = {0}) AND following_id != {0} GROUP BY user.id, avatar, username".format(user_id[0])
+	cursor.execute(get_trending_users_query)
+	trending_users_result = cursor.fetchall()
+
+	return jsonify(trending_users_result)
+
+@app.route('/follow', methods=['POST'])
+def follow():
+	data = request.get_json()
+	username = data['username']
+	following_id = data['following_id']
+
+	get_user_id_query = "SELECT id FROM user WHERE username = '%s'" % username
+	cursor.execute(get_user_id_query)
+	user_id = cursor.fetchone()
+
+	follow_query = "INSERT INTO follow (follower_id, following_id) VALUES ('%s', '%s')" % (user_id[0], following_id)
+	print follow_query
+	cursor.execute(follow_query)
+	conn.commit()
+
+	return "yay!"
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- static/scripts/mainController.js
+++ static/scripts/mainController.js
@@ -12,14 +12,52 @@ bawkApp.config(function($locationProvider) {
 
 bawkApp.controller('mainController', function($scope, $http, $location, $cookies){
 
+	// auto resize text area
+	// found @ http://stackoverflow.com/questions/23818131/dynamically-expand-height-of-input-type-text-based-on-number-of-characters-typ
+	var span = $('<span>').css('display', 'inline-block')
+	    .css('word-break', 'break-all')
+	    .appendTo('body').css('visibility', 'hidden');
+	function initSpan(textarea) {
+	    span.text(textarea.text())
+	        .width(textarea.width())
+	        .css('font', textarea.css('font'));
+	}
+	$('textarea').on({
+	    input: function() {
+	        var text = $(this).val();
+	        span.text(text);
+	        $(this).height(text ? span.height() : '1.1em');
+	    },
+	    focus: function() {
+	        initSpan($(this));
+	    },
+	    keypress: function(e) {
+	        //cancel the Enter keystroke, otherwise a new line will be created
+	        //This ensures the correct behavior when user types Enter 
+	        //into an input field
+	        if (e.which == 13) e.preventDefault();
+	    }
+	});
+
+
 	checkToken()
 
-	if ($location.path() == '/'){
-		$http.get('http://localhost:5000/get_posts', {
 
+
+	if ($location.path() == '/'){
+		// load posts
+		$http.post('http://localhost:5000/get_posts', {
+			username: $scope.username
 		}).then(function success(response){
-			$scope.posts = response.data
-			// console.log($scope.posts)
+			$scope.posts = response.data;
+
+		})
+		// get trending users
+		$http.post('http://localhost:5000/get_trending_users', {
+			username: $scope.username
+		}).then(function success(response){
+			$scope.users = response.data;
+
 		})
 	
 	}
@@ -29,10 +67,13 @@ bawkApp.controller('mainController', function($scope, $http, $location, $cookies
 		$http.post('http://localhost:5000/register_submit', {
 			username: $scope.username,
 			password: $scope.password,
-			email: $scope.email
-
+			avatar: $scope.avatar
+		}).then(function success(response){
+			if(response.data == 'reg successful'){
+				$scope.loggedIn = true;
+			}
 		})
-		console.log('register')
+
 	}
 
 	$scope.login = function(){
@@ -40,10 +81,12 @@ bawkApp.controller('mainController', function($scope, $http, $location, $cookies
 			username: $scope.username,
 			password: $scope.password
 		}).then(function successCallback(response){
-			if (response.data == "true"){
+			if (response.data){
 				$scope.loggedIn = true;
 				$scope.signedInAs = $scope.username;
 				$cookies.put('username', $scope.username);
+				$scope.avatar = response.data;
+				$cookies.put('avatar', $scope.avatar)
 			}
 		})
 	}
@@ -51,16 +94,56 @@ bawkApp.controller('mainController', function($scope, $http, $location, $cookies
 	function checkToken() {
 		if($cookies.get('username')) {
 			$scope.username = $cookies.get('username');
+			$scope.avatar = $cookies.get('avatar');
 			$scope.loggedIn = true;
 			$scope.signedInAs = $scope.username;
-			$http.post('http://localhost:5000/')	
+			// $http.post('http://localhost:5000/')	
 		}
 	}
 
 	$scope.logout = function(){
-		$cookies.remove('token');
+		$cookies.remove('username');
+		$cookies.remove('avatar');
 		$scope.signedInAs = null;
 		$scope.loggedIn = false;
+	}
+
+	$scope.makePost = function(){
+		newPostContent = $scope.newPostInput;
+		$scope.newPostInput = '';
+
+		$http.post('http://localhost:5000/new_post', {
+			newPostContent: newPostContent,
+			username: $scope.username
+		}).then(function success(response){
+			$http.get('http://localhost:5000/get_posts', {
+
+			}).then(function success(response){
+			$scope.posts = response.data
+
+			})
+		})
+	}
+
+	$scope.follow = function(){
+		// $http.post('http://localhost:5000/follow', {
+		// 	username: $scope.username,
+
+		// })
+	}
+
+	$scope.followUser = function(id, index){
+		$scope.users.splice(index, 1)
+		if ($scope.users.length == 0){
+			$scope.everyoneFollowed = true;
+		}
+		console.log(id)
+
+		$http.post('http://localhost:5000/follow', {
+			username: $scope.username,
+			following_id: id
+		})
+
 	}
 
 
